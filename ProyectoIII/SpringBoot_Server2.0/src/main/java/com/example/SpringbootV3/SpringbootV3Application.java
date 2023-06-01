73ccd08200eb0ea6341387bc4be94c28f17d3ddc
yonathan_ms146 <yonathan0146@estudiantec.cr>
2023-06-01 00:01:28 -0600

    Spring Boot cambios
    Se agregó un nuevo spring boot con nuevas dependencias

---
+++ ProyectoIII/SpringBoot_Server2.0/src/main/java/com/example/SpringbootV3/SpringbootV3Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringbootV3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootV3Application.class, args);
+	}
+
+}
---
+++ ProyectoIII/SpringBoot_Server2.0/src/test/java/com/example/SpringbootV3/SpringbootV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringbootV3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
