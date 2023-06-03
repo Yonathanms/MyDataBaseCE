1e1bf0d57ae902260421a58d52effac7f2f18b02
yonathan_ms146 <yonathan0146@estudiantec.cr>
2023-06-03 17:45:30 -0600

    MVC_APIRest
    Se inicia con la estructura del patrón de diseño Modelo-Vista-Controlador, además de una clase persona como clase prueba

---
+++ ProyectoIII/SpringBoot_Server2.0/src/main/java/Repositorios/PersonaRepositorioApi.java
@@ -0,0 +1,7 @@
+package Repositorios;
+import Modelo.Persona;
+
+//Esta importacion es del crud y es como un tipo de frameworkweb pero local, hace la tarea del Vue
+import org.springframework.data.repository.CrudRepository;
+public interface PersonaRepositorioApi extends CrudRepository<Persona, Long> {
+}
